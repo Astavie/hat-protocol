42534e32477f04f6a4504d3641e1e9fc65916525
Astavie <astavie@pm.me>
2024-02-13 20:13:33 +0100

    Deno args

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import { Connection } from "./actor/connection.ts";
 import { getIP } from "https://deno.land/x/get_ip@v2.0.0/mod.ts";
 import { Actor, Address, System } from "./actor/types.ts";
-import { getNetworkAddr } from "https://deno.land/x/local_ip@0.0.3/mod.ts";
 
 class MessageLog extends Actor {
   messages: string[] = [];
@@ -28,9 +27,13 @@ async function asyncPrompt(question: string): Promise<string> {
 }
 
 if (import.meta.main) {
-  const hostname = await getNetworkAddr() ?? "";
+  if (!Deno.args[1] || !Deno.args[2]) {
+    console.log(`USE: ${Deno.args[0]} <local ip> <remote ip>`)
+    Deno.exit()
+  }
+
   const publicname = await getIP()
-  const conn = new Connection(hostname, publicname, 53706);
+  const conn = new Connection(Deno.args[1], publicname, 53706);
 
   const log = new MessageLog()
   log.uuid = "log";
@@ -38,9 +41,8 @@ if (import.meta.main) {
   const addr = conn.add(log)
   console.log("Server host: " + addr.host);
 
-  const host = prompt("Remote host:") ?? "";
   const remote: Address<MessageLog> = {
-    host,
+    host: Deno.args[2],
     uuid: "log",
   }
 
